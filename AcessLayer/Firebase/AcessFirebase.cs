cd9d713cd8b487e0aac0dceac4e8e7c7874f3a57
emanuel <emanuel@enotas.com.br>
2021-05-19 10:38:26 -0300

    Atualiações no arquivo gitignore

--- AcessLayer/Firebase/AcessFirebase.cs
+++
@@ -1,9 +0,0 @@
-﻿using Firebase.Database;
-
-namespace AcessLayer.Firebase
-{
-    public class AcessFirebase
-    {
-        public static FirebaseClient firebase = new FirebaseClient("https://bookshelf-11701.firebaseio.com/");
-    }
-}
